c9690feafb78325607bb7cfe5f1e852d5876b702
Coos Baakman <coos.baakman@radboudumc.nl>
2021-06-28 09:29:08 +0000

    add more tests

--- test/features/test_atomic_contacts.py
+++ test/features/test_atomic_contacts.py
@@ -6,8 +6,53 @@ import shutil
 import numpy
 from nose.tools import ok_, eq_
 
-from deeprank.features.atomic_contacts import __compute_feature__, _PhysicsStorage
+from deeprank.features.atomic_contacts import __compute_feature__, _PhysicsStorage, AtomicContacts
 from deeprank.models.variant import PdbVariantSelection
+
+
+def test_assign_parameters():
+    pdb_path = "test/1CRN.pdb"
+    forcefield_path = "deeprank/features/forcefield"
+    top_path = os.path.join(forcefield_path, 'protein-allhdg5-4_new.top')
+    param_path = os.path.join(forcefield_path, 'protein-allhdg5-4_new.param')
+    patch_path = os.path.join(forcefield_path, 'patch.top')
+
+    variant = PdbVariantSelection(pdb_path, "A", None, None)  # don't care about the amino acid change
+
+    with AtomicContacts(variant, top_path, param_path, patch_path) as feature_obj:
+
+        feature_obj._read_top()
+        feature_obj._read_param()
+        feature_obj._read_patch()
+        feature_obj._assign_parameters()
+
+        # Check that glutamate has a negative charge
+        charge = feature_obj.sqldb.get("CHARGE", resSeq=23, resName="GLU", name="OE2")[0]
+        ok_(charge < 0.0)
+
+        # Check that arginine has a positive charge
+        charge = feature_obj.sqldb.get("CHARGE", resSeq=10, resName="ARG", name="CZ")[0]
+        ok_(charge > 0.0)
+
+        # Check that the N-terminal CA has a positive charge
+        charge = feature_obj.sqldb.get("CHARGE", resSeq=1, resName="THR", name="CA")[0]
+        ok_(charge > 0.0)
+
+        # Check that the phenylalanine ring is neutral
+        charge = feature_obj.sqldb.get("CHARGE", resSeq=13, resName="PHE", name="CZ")[0]
+        eq_(charge, 0.0)
+
+        # Check that the vanderwaals parameters are all within the 0.0 - 4.0 range
+        for inter_epsilon, inter_sigma, intra_epsilon, intra_sigma in \
+                feature_obj.sqldb.get("inter_epsilon, inter_sigma, intra_epsilon, intra_sigma"):
+
+            min_ = 0.0
+            max_ = 4.0
+
+            ok_(inter_epsilon > min_ and inter_epsilon < max_)
+            ok_(inter_sigma > min_ and inter_sigma < max_)
+            ok_(intra_epsilon > min_ and intra_epsilon < max_)
+            ok_(intra_sigma > min_ and intra_sigma < max_)
 
 
 def test_compute_feature():
@@ -60,6 +105,10 @@ def test_physics():
     vdw_short = _PhysicsStorage.get_vanderwaals_energy(eps, sig, eps, sig, 7.0)
     vdw_long = _PhysicsStorage.get_vanderwaals_energy(eps, sig, eps, sig, 9.0)
 
+    # if distance > sigma, then LJ energy should be negative and less negative at longer distance
+
+    ok_(vdw_short <= 0.0)
+    ok_(vdw_long <= 0.0)
     ok_(vdw_short < vdw_long)
 
     charge1 = -1.0
@@ -67,12 +116,22 @@ def test_physics():
     charge3 = 1.0
     max_dist = 20.0
 
+    # two opposing charges should attract each other
+    # so coulomb energy should be negative and less negative at longer distance
+
     c_short = _PhysicsStorage.get_coulomb_energy(charge1, charge2, 5.0, max_dist)
     c_long = _PhysicsStorage.get_coulomb_energy(charge1, charge2, 10.0, max_dist)
 
+    ok_(c_short < 0.0)
+    ok_(c_long < 0.0)
     ok_(c_short < c_long)
+
+    # two opposing charges should repulse each other
+    # so coulomb energy should be positive and less positive at longer distance
 
     c_short = _PhysicsStorage.get_coulomb_energy(charge3, charge2, 5.0, max_dist)
     c_long = _PhysicsStorage.get_coulomb_energy(charge3, charge2, 10.0, max_dist)
 
+    ok_(c_short > 0.0)
+    ok_(c_long > 0.0)
     ok_(c_short > c_long)
