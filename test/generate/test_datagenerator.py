b86616ee4426619ad4f4330a3016538fc6fc7354
cbaakman <c.baakman@umcn.nl>
2022-01-27 14:04:57 +0100

    fix the environment

--- test/generate/test_datagenerator.py
+++ test/generate/test_datagenerator.py
@@ -1,8 +1,12 @@
 from deeprank.models.variant import PdbVariantSelection
 from deeprank.domain.amino_acid import *
 from deeprank.generate.DataGenerator import DataGenerator
+from deeprank.models.environment import Environment
 
 
 def test_get_grid_center():
-    variant = PdbVariantSelection("test/data/101m.pdb", "A", 25, glycine, alanine)
-    DataGenerator.get_grid_center(variant)
+    environment = Environment(pdb_root="test/data/pdb", device="cpu")
+
+    variant = PdbVariantSelection("101m", "A", 25, glycine, alanine)
+
+    DataGenerator.get_grid_center(environment, variant)
