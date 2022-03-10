80d18817857c919efb0896171fb6b12587a0cc08
Coos Baakman <cbaakman@radboudumc.nl>
2022-03-10 15:07:42 +0100

    to fix the unit tests

--- deeprank/features/atomic_contacts.py
+++ deeprank/features/atomic_contacts.py
@@ -35,7 +35,10 @@ def _store_features(feature_group_xyz, feature_group_raw, feature_name, atoms, v
 
     data = [list(atoms[index].position) + [values[index]] for index in range(len(atoms))]
 
-    feature_group_xyz.create_dataset(feature_name, data=data, compression="lzf", chunks=(100,4))
+    if data.shape[0] > 100:
+        feature_group_xyz.create_dataset(feature_name, data=data, compression="lzf", chunks=(100,4))
+    else:
+        feature_group_xyz.create_dataset(feature_name, data=data, compression="lzf")
 
     # We're currently not doing anything with the raw features.
 
