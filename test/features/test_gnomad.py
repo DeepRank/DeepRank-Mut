d2aea137a4a5edcc425348573a5cf853ddfe9e31
Coos Baakman <cbaakman@radboudumc.nl>
2022-08-22 09:49:47 +0200

    works with links too

--- test/features/test_gnomad.py
+++ test/features/test_gnomad.py
@@ -35,3 +35,30 @@ def test_dbnsfp():
         assert numpy.any(data == 0.0)
 
     os.remove(tmp_path)
+
+
+def test_linked_dbnsfp():
+
+    environment = Environment(pdb_root="test/data/pdb",
+                              gnomad_path="test/data/link_gnomAD.hdf5")
+    variant = PdbVariantSelection(pdb_ac="1KLR", chain_id="A",
+                                  residue_number=94,
+                                  wildtype_amino_acid=serine, variant_amino_acid=cysteine,
+                                  enst_accession='ENST00000625061',
+                                  protein_accession='P08048',
+                                  protein_residue_number=94)
+
+    tmp_file, tmp_path = tempfile.mkstemp()
+    os.close(tmp_file)
+
+    with h5py.File(tmp_path, 'w') as f5:
+
+        test_group = f5.require_group("test")
+
+        __compute_feature__(environment, 0.0, test_group, variant)
+
+        data = test_group[AF_FEATURE_NAME][()]
+
+        assert numpy.any(data == 0.0)
+
+    os.remove(tmp_path)
